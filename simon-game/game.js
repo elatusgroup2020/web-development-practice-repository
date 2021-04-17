9fe926e4233c26351f932400d93dc90a0884a257
elatusgroup2020 <75922452+elatusgroup2020@users.noreply.github.com>
2021-04-17 22:46:14 +0800

    Add files via upload

---
+++ simon-game/game.js
@@ -0,0 +1,77 @@
+var randomNumber;
+var buttonColors = ["red", "blue", "green", "yellow"];
+var randomChosenColor;
+var gamePattern = [];
+var userClickedPattern = [];
+var level = 1;
+var result = true;
+var gameStart = false;
+var validation = false;
+
+function nextSequence() {
+	$("h1").text("Level "+level);
+	randomNumber = Math.floor(Math.random() * 4);
+	randomChosenColor = buttonColors[randomNumber];
+	gamePattern.push(randomChosenColor);
+	$("#"+randomChosenColor).fadeOut(150).fadeIn(150);
+	playSound(randomChosenColor);
+	level++;
+}
+
+function playSound(name) {
+	var audio = new Audio("sounds/"+name+".mp3");
+	audio.play();
+}
+
+function animatePress(currentColor) {
+	$("#"+currentColor).addClass("pressed");
+	setTimeout(function () {
+		$("#"+currentColor).removeClass("pressed");
+	}, 100);
+}
+
+function gameOver() {
+	$("h1").text("Game Over, Press Any Key to Restart");
+	playSound("wrong");
+	$("body").addClass("game-over");
+	setTimeout(function () {
+		$("body").removeClass("game-over");
+		gamePattern = [];
+		userClickedPattern = [];
+		level = 1;
+		result = true;
+		gameStart = false;
+	}, 100);
+}
+
+$(".btn").on("click",function(){
+	if(gameStart == true && validation == false) {
+		var userChosenColor = this.id;
+		userClickedPattern.push(userChosenColor);	
+		animatePress(userChosenColor);
+		playSound(userChosenColor);
+		for(var ctr=0;ctr<userClickedPattern.length;ctr++) {
+			if(userClickedPattern[ctr] != gamePattern[ctr]) {
+				result = false;
+			}
+		}
+		if(result === false) {
+			gameOver();
+		}
+		if(result === true && gamePattern.length > 0 && userClickedPattern.length === gamePattern.length) {
+			validation = true;
+			setTimeout(function() {
+				userClickedPattern = [];
+				nextSequence();
+				validation = false;
+			}, 500);
+		}
+	}
+});
+
+$(document).on("keypress",function(){
+	if(gameStart === false) {
+		gameStart = true;
+		nextSequence();
+	}
+});
