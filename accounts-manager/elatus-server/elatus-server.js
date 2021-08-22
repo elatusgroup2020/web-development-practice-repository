805dd9cd2ef8e41bd2ecf58718af16e4285377b1
Bani Home PC <>
2021-08-23 01:03:39 +0800

    updated accounts manager

--- accounts-manager/elatus-server/elatus-server.js
+++ accounts-manager/elatus-server/elatus-server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const ejs = require("ejs");
 const bodyParser = require("body-parser");
+const https = require("https");
 const mongoose = require("mongoose");
 
 const app = express();
@@ -9,41 +10,206 @@ app.set("view engine", "ejs");
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({extended:true}));
 
-mongoose.connect("mongodb://localhost:27017/accounts-manager-db", {useNewUrlParser:true});
-
-const accountSchema = new mongoose.Schema({
-	account : String,
-	username : String,
-	password : String
-});
-
-const Account = mongoose.model("account", accountSchema);
-
 let globalVar1 = "";
 
 app.get("/accounts-manager",function(req,res){
 	res.render("accounts-manager/index");
 });
 
-app.post("/accounts-manager",function(req,res){
-	let dataFromServer = {
-		account : "a",
-		username : "b",
-		password : "c"
-	};
+app.post("/accounts-manager-retrieve",function(req,res){
+	mongoose.connect("mongodb://localhost:27017/accounts-manager-db", {useNewUrlParser:true,useUnifiedTopology:true});
 	
-	Account.find(function(err,accounts){
+	const accountSchema = new mongoose.Schema({
+		account : String,
+		username : String,
+		password : String
+	});
+
+	const Account = mongoose.model("Account", accountSchema);
+	
+	Account.find({"account":{"$regex":req.body.keyword,"$options":"i"}},function(err,accounts){
+		const dataToClient = {
+			account : "",
+			username : "",
+			password : "",
+			status : ""
+		};
+		
 		if(err){
-			console.log(err);
+			dataToClient.status = "no data";
+		} else if(accounts.length < 1) {
+			dataToClient.status = "no data";
+		} else if(accounts.length > 1) {
+			accounts.forEach(function(account){
+				dataToClient.account = dataToClient.account + "-> " + account.account + "\n";
+			});
+			dataToClient.status = "many data";
 		} else {
-			dataFromServer.account = accounts[0].account;
-			dataFromServer.username = accounts[0].username;
-			dataFromServer.password = accounts[0].password;			
+			dataToClient.account = accounts[0].account;
+			dataToClient.username = accounts[0].username;
+			dataToClient.password = accounts[0].password;			
 		}
 		
-		res.send(dataFromServer);
+		delete mongoose.connection.models['Account'];
+		mongoose.connection.close()
+		
+		res.send(dataToClient);
 	});	
+});
 
+app.post("/accounts-manager-save",function(req,res){
+	mongoose.connect("mongodb://localhost:27017/accounts-manager-db", {useNewUrlParser:true,useUnifiedTopology:true});
+	
+	const accountSchema = new mongoose.Schema({
+		account : String,
+		username : String,
+		password : String
+	});
+
+	const Account = mongoose.model("Account", accountSchema);
+	
+	Account.find({account:req.body.account},function(err,accounts){
+		const dataToClient = {
+			status : ""
+		};
+		
+		if(err){
+			dataToClient.status = "error";
+			
+			delete mongoose.connection.models['Account'];
+			mongoose.connection.close()
+			
+			res.send(dataToClient);
+		} else if(accounts.length > 0) {
+			dataToClient.status = "duplicate";
+			
+			delete mongoose.connection.models['Account'];
+			mongoose.connection.close()
+		
+			res.send(dataToClient);
+		} else {
+			const account = new Account({
+				account : req.body.account,
+				username : req.body.username,
+				password : req.body.password
+			});
+			
+			account.save(function(err){
+				if(err){
+					dataToClient.status = "error";
+				} else {
+					dataToClient.status = "success";
+				}
+				
+				delete mongoose.connection.models['Account'];
+				mongoose.connection.close()
+				
+				res.send(dataToClient);
+			});
+		}
+	});
+});
+
+app.post("/accounts-manager-update",function(req,res){
+	mongoose.connect("mongodb://localhost:27017/accounts-manager-db", {useNewUrlParser:true,useUnifiedTopology:true});
+	
+	const accountSchema = new mongoose.Schema({
+		account : String,
+		username : String,
+		password : String
+	});
+
+	const Account = mongoose.model("Account", accountSchema);
+	
+	Account.find({account:req.body.account},function(err,accounts){
+		const dataToClient = {
+			status : ""
+		};
+		
+		if(err){
+			dataToClient.status = "no data";
+			
+			delete mongoose.connection.models['Account'];
+			mongoose.connection.close()
+			
+			res.send(dataToClient);
+		} else if(accounts.length > 0) {	
+			if(req.body.username != "") {
+				accounts[0].username = req.body.username;
+			}
+			
+			if(req.body.password != "") {
+				accounts[0].password = req.body.password;
+			}
+			
+			accounts[0].save(function(err){
+				if(err){
+					dataToClient.status = "error";
+				} else {
+					dataToClient.status = "success";
+				}
+				
+				delete mongoose.connection.models['Account'];
+				mongoose.connection.close()
+				
+				res.send(dataToClient);
+			});
+		} else {
+			dataToClient.status = "no data";
+			
+			delete mongoose.connection.models['Account'];
+			mongoose.connection.close()
+		
+			res.send(dataToClient);
+		}
+	});
+});
+
+app.post("/accounts-manager-delete",function(req,res){
+	mongoose.connect("mongodb://localhost:27017/accounts-manager-db", {useNewUrlParser:true,useUnifiedTopology:true});
+	
+	const accountSchema = new mongoose.Schema({
+		account : String,
+		username : String,
+		password : String
+	});
+
+	const Account = mongoose.model("Account", accountSchema);
+	
+	Account.find({account:req.body.account},function(err,accounts){
+		const dataToClient = {
+			status : ""
+		};
+		
+		if(err){
+			dataToClient.status = "no data";
+			
+			delete mongoose.connection.models['Account'];
+			mongoose.connection.close()
+			
+			res.send(dataToClient);
+		} else if(accounts.length > 0) {	
+			Account.deleteOne({account:req.body.account},function(err){
+				if(err){
+					dataToClient.status = "error";
+				} else {
+					dataToClient.status = "success";
+				}
+				
+				delete mongoose.connection.models['Account'];
+				mongoose.connection.close()
+				
+				res.send(dataToClient);
+			});
+		} else {
+			dataToClient.status = "no data";
+			
+			delete mongoose.connection.models['Account'];
+			mongoose.connection.close()
+		
+			res.send(dataToClient);
+		}
+	});
 });
 
 app.listen(3000,function(){
--- accounts-manager/elatus-server/public/accounts-manager/js/scripts.js
+++ accounts-manager/elatus-server/public/accounts-manager/js/scripts.js
@@ -1,9 +1,6 @@
-let jqxhr;// = $.post( "", {} );
-let dummyDataFromServer = {
-		account : "",
-		username : "",
-		password : ""
-	};
+let jqxhr;
+
+$("#loading-result").css("visibility", "hidden");
 
 $("#show-password").on("click",function(){
 	if($("#password").attr("type") === "password"){
@@ -14,27 +11,165 @@ $("#show-password").on("click",function(){
 });
 
 $("#retrieve").on("click",function(){
-	if($("#code").val() === "") {
-		alert("CODE field is required");
-	} else {
-		//send ajax request to server
-		jqxhr = $.post( "/accounts-manager", {} );
+	$("#loading-result").css("visibility", "visible");
+	$("input").attr("disabled","disabled");
+	$("button").attr("disabled","disabled");
+	
+	jqxhr = $.post( "/accounts-manager-retrieve", {keyword:$("#account").val()} );
+	
+	$("#account").val("");
+	$("#username").val("");
+	$("#password").val("");
+	
+	jqxhr.done(function(data){
+		$("#loading-result").css("visibility", "hidden");
+		$("input").removeAttr("disabled");
+		$("button").removeAttr("disabled");
 		
-		//receive data from server
-		jqxhr.done(function(data){
-			dummyDataFromServer.account = data.account;
-			dummyDataFromServer.username = data.username;
-			dummyDataFromServer.password = data.password;
-			
-			//verify data from server
+		if(data.status === "no data") {
+			alert("No data found.");
+		} else if(data.status === "many data") {
+			alert("Please specify from the list of retrieved data :\n" + data.account);
+		} else {
+			$("#account").val(data.account);
+			$("#username").val(data.username);
+			$("#password").val(data.password);
+		}
+	});
+	
+	jqxhr.fail(function(){
+		$("#loading-result").css("visibility", "hidden");
+		$("input").removeAttr("disabled");
+		$("button").removeAttr("disabled");
+		
+		alert("Cannot retrieve data from server as of the moment.");
+	});
+});
 
-			//update the ui fields
-			$("#account").attr("value",dummyDataFromServer.account);
-			$("#username").attr("value",dummyDataFromServer.username);
-			$("#password").attr("value",dummyDataFromServer.password);
+$("#save").on("click",function(){
+	$("#loading-result").css("visibility", "visible");
+	$("input").attr("disabled","disabled");
+	$("button").attr("disabled","disabled");
+	
+	if($("#account").val() === "" || $("#username").val() === "" || $("#password").val() === "") {
+		alert("Need to fill up all the fields.");
+	} else {
+		jqxhr = $.post( 
+			"/accounts-manager-save", 
+			{
+				account : $("#account").val(),
+				username : $("#username").val(),
+				password : $("#password").val()
+			}
+		);
+		
+		$("#account").val("");
+		$("#username").val("");
+		$("#password").val("");
+		
+		jqxhr.done(function(data){
+			$("#loading-result").css("visibility", "hidden");
+			$("input").removeAttr("disabled");
+			$("button").removeAttr("disabled");
+			
+			if(data.status === "error") {
+				alert("Data not saved.");
+			} else if(data.status === "duplicate") {
+				alert("Data already exists.");
+			} else {
+				alert("Data saved.");
+			}
 		});
+		
 		jqxhr.fail(function(){
-			alert("I HAVE FAILED!");
+			$("#loading-result").css("visibility", "hidden");
+			$("input").removeAttr("disabled");
+			$("button").removeAttr("disabled");
+			
+			alert("Cannot save data to server as of the moment.");
+		});
+	}
+});
+
+$("#update").on("click",function(){
+	$("#loading-result").css("visibility", "visible");
+	$("input").attr("disabled","disabled");
+	$("button").attr("disabled","disabled");
+	
+	if($("#account").val() === "" || ($("#username").val() === "" && $("#password").val() === "")) {
+		alert("Need to fill up the Account field.\nNeed to fill up atleast one of Username and Password fields.");
+	} else {
+		jqxhr = $.post( 
+			"/accounts-manager-update", 
+			{
+				account : $("#account").val(),
+				username : $("#username").val(),
+				password : $("#password").val()
+			}
+		);
+		
+		$("#account").val("");
+		$("#username").val("");
+		$("#password").val("");
+		
+		jqxhr.done(function(data){
+			$("#loading-result").css("visibility", "hidden");
+			$("input").removeAttr("disabled");
+			$("button").removeAttr("disabled");
+			
+			if(data.status === "error") {
+				alert("Data not updated.");
+			} else if(data.status === "no data") {
+				alert("Data does not exist.");
+			} else {
+				alert("Data updated.");
+			}
+		});
+		
+		jqxhr.fail(function(){
+			$("#loading-result").css("visibility", "hidden");
+			$("input").removeAttr("disabled");
+			$("button").removeAttr("disabled");
+			
+			alert("Cannot update data of server as of the moment.");
+		});
+	}
+});
+
+$("#delete").on("click",function(){
+	$("#loading-result").css("visibility", "visible");
+	$("input").attr("disabled","disabled");
+	$("button").attr("disabled","disabled");
+	
+	if($("#account").val() === "") {
+		alert("Need to fill up the Account field.");
+	} else {
+		jqxhr = $.post( "/accounts-manager-delete", {account:$("#account").val()} );
+		
+		$("#account").val("");
+		$("#username").val("");
+		$("#password").val("");
+		
+		jqxhr.done(function(data){
+			$("#loading-result").css("visibility", "hidden");
+			$("input").removeAttr("disabled");
+			$("button").removeAttr("disabled");
+			
+			if(data.status === "error") {
+				alert("Data not deleted.");
+			} else if(data.status === "no data") {
+				alert("Data does not exist.");
+			} else {
+				alert("Data deleted.");
+			}
+		});
+		
+		jqxhr.fail(function(){
+			$("#loading-result").css("visibility", "hidden");
+			$("input").removeAttr("disabled");
+			$("button").removeAttr("disabled");
+			
+			alert("Cannot delete data of server as of the moment.");
 		});
 	}
 });
